28a1de23b6c4070fe00ff496ffa68e0e7ace4b18
InsonusK <insonus.k@gmail.com>
2023-06-15 22:19:39 +0000

    bugfix

--- src/client/usa_stock_market/exceptions.py
+++ src/client/usa_stock_market/exceptions.py
@@ -3,6 +3,6 @@ from .columns.column_list import Column, ColType
 
 
 class WrongColumnsException(Exception):
-  def __init__(self, wrongColList: List[Column], type: ColType) -> None:
+  def __init__(self, wrongColList: List[str], type: ColType) -> None:
     super().__init__(
-        f"Column{'s' if len(wrongColList) > 1 else ''} doesn't allowed for {type.value}: {', '.join([c.output_name for c in wrongColList])}")
+        f"Column{'s' if len(wrongColList) > 1 else ''} doesn't allowed for {type.value}: {', '.join(wrongColList)}")
